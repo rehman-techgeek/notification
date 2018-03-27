da092df9bfe03cf1ea9621cb3f7475381d4fa342
Abdul Rehman, Mohammad <armohamm@ftdi.com>
2018-03-27 09:57:08 +0530

    Initial Commit

---
+++ src/main/java/com/inetsolv/Receiver.java
@@ -0,0 +1,26 @@
+package com.inetsolv;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+class Receiver {
+	
+	@Autowired
+	Mailer mailer;
+
+	@Bean
+	Queue queue() {
+		return new Queue("CustomerQ", false);
+	}
+	
+	@RabbitListener(queues = "CustomerQ")
+    public void processMessage(String email) {
+       System.out.println(email);
+       mailer.sendMail(email);
+    }
+	
+}
